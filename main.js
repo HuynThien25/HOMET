980784699920f165be8e580efa16cb54ecbc8653
HuynThien25 <thien25102@gmail.com>
2024-06-03 15:06:40 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,77 @@
+const scrollRevealOption={
+    distance:"50px",
+    origin:"bottom",
+    duration:1000,
+};
+
+//header container
+ScrollReveal().reveal(".header__container img",{
+    duration:1000,
+});
+ScrollReveal().reveal(".header__container h1",{
+    ...scrollRevealOption,
+    delay:500,
+});
+ScrollReveal().reveal(".header__container p",{
+    ...scrollRevealOption,
+    delay:1000,
+});
+ScrollReveal().reveal(".header__btns",{
+    ...scrollRevealOption,
+    delay:1500,
+});
+//about container
+ScrollReveal().reveal(".about__image img",{
+    ...scrollRevealOption,
+    origin:"left",
+});
+
+ScrollReveal().reveal(".about__content h3",{
+    ...scrollRevealOption,
+    delay:500,
+});
+ScrollReveal().reveal(".about__content .section__header",{
+    ...scrollRevealOption,
+    delay:1000,
+});
+ScrollReveal().reveal(".about__content .section__subheader",{
+    ...scrollRevealOption,
+    delay:1500,
+});
+ScrollReveal().reveal(".about__content .about__grid",{
+    ...scrollRevealOption,
+    delay:2000,
+});
+//contact container
+ScrollReveal().reveal(".contact__image img",{
+    ...scrollRevealOption,
+    origin: "left",
+})
+
+const swiper=new Swiper(".swiper",{
+    loop:true,
+    slidesPerView:"auto",
+    centeredSlides:true,
+    spaceBetween:30,
+});
+
+// const swiper = new Swiper('.swiper', {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   // using "ratio" endpoints
+//   breakpoints: {
+//     '@0.75': {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//       spaceBetween:20,
+//     },
+//     '@1.00': {
+//       slidesPerView: 3,
+//       spaceBetween: 40,
+//     },
+//     '@1.50': {
+//       slidesPerView: 4,
+//       spaceBetween: 50,
+//     },
+//   }
+// });
